14035d9d91b90c11c7cb4df2f118a86b009e60a6
Jessica Dyer <jessica.dyer_intern@leveltenenergy.com>
2022-10-03 06:14:26 -0700

    set up epi 510 hw

---
+++ hw_1/vipcls data.R
@@ -0,0 +1,99 @@
+## ---------------------------
+## Script name: Epi 510 HW 1
+##
+## Purpose of script:
+##
+## Author: 
+##
+## Date Created: 
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+
+## Packages
+library(tidyverse)
+class(vipcls)
+data.frame()
+view(vipcls)
+names(vipcls)
+dim(vipcls)
+str(vipcls)
+summary(vipcls)
+
+## Import data
+vipcls <- read.csv("vipcls.csv")
+
+## Change WD to Epi 510
+current_wd = getwd()
+epi_wd = "/Repositories/epi_510/hw_1"
+wd = paste(current_wd, epi_wd, sep = "")
+setwd(paste(current_wd, epi_wd, sep=""))
+
+## Create a dataframe: 
+
+
+#Set missing data to NA
+vipcls[vipcls==-1] <- NA
+#Set mother’s age (momage) of ≤15 to missing 
+vipcls$momage[vipcls$momage<=15] <- NA
+#Set gestational age (delges) above 52 to missing 
+vipcls$delges[vipcls$delges>52] <- NA
+#Set birth weight of babies (bw) below 300 to missing 
+vipcls$bw[vipcls$bw<300] <- NA
+#Set birth weight of babies (bw) 6000 and above to missing 
+vipcls$bw[vipcls$bw>=6000] <- NA
+#Convert binary variables to logicals. 
+  #Step 1: Convert deltype cesarean (2) to 0 (false) and convert to logical
+  vipcls$deltype[vipcls$deltype==2] <- 0
+  vipcls$deltype <- as.logical(vipcls$deltype) 
+  #Step 2: Convert 2 (No) to 0 (false) for induclab and convert to logical
+  vipcls$induclab[vipcls$induclab==2] <- 0
+  vipcls$induclab <- as.logical(vipcls$induclab) 
+  #Step 3: Convert 2 (No) to 0 (false) for auglab and convert to logical
+  vipcls$auglab[vipcls$auglab==2] <- 0
+  vipcls$auglab <- as.logical(vipcls$auglab) 
+  #Step 4: Convert 2 (No) to 0 (false) for intrapih and convert to logical
+  vipcls$intrapih[vipcls$intrapih==2] <- 0
+  vipcls$intrapih <- as.logical(vipcls$intrapih) 
+#Convert categorical variables w/ more than 2 levels to factors
+  #Step 1: Convert etoh 1
+  vipcls$etoh1  <-  factor(vipcls$etoh1, levels = 1:6, labels=c("every day", "3-5/wk", "1/wk","<1/wk", "<1/month", 	"never"))	
+  #Step 2: Convert etoh 2
+  vipcls$etoh2  <-  factor(vipcls$etoh2, levels = 1:6, labels=c("every day", "3-5/wk", "1/wk","<1/wk", "<1/month", 	"never"))	
+  #Step 3: Convert marstat
+  vipcls$marstat  <-  factor(vipcls$marstat, levels = 1:5, labels=c("married", "separated", "divorced","widowed", "never married"))	
+  #Step 4: Convert raceth
+  vipcls$raceth  <-  factor(vipcls$raceth, levels = 0:2, labels=c("White", "Hispanic", "Black"))	
+#determine the minimum, maximum, mean and the number of missing values for the numeric variables & levels, # observations and the number of missing values for binary/categorical variables
+  summary(vipcls)
+#percentage of mothers drank alcohol exactly one time per week during their second trimester
+  table(vipcls$etoh2)
+  tableA <- table(vipcls$etoh2)
+  prop.table(tableA)*100
+#calculate the median
+  median(vipcls$grade, na.rm = TRUE)
+#calculate the 95th percentile of years of education
+  quantile(vipcls$grade, c(0, .1, .5, .75, .95, 1), na.rm = TRUE)
+#Calculate  mean, standard deviation, and sample sizes for mother’s age in each of the five marital groups
+  mean(vipcls$momage[vipcls$marstat=="married"], na.rm = TRUE) 
+  sd(vipcls$momage[vipcls$marstat=="married"], na.rm = TRUE) 
+  quantile(vipcls$momage[vipcls$marstat=="married"], c(1), na.rm = TRUE)
+  mean(vipcls$momage[vipcls$marstat=="separated"], na.rm = TRUE) 
+  sd(vipcls$momage[vipcls$marstat=="separated"], na.rm = TRUE) 
+  quantile(vipcls$momage[vipcls$marstat=="separated"], c(1), na.rm = TRUE)
+  mean(vipcls$momage[vipcls$marstat=="divorced"], na.rm = TRUE) 
+  sd(vipcls$momage[vipcls$marstat=="divorced"], na.rm = TRUE) 
+  quantile(vipcls$momage[vipcls$marstat=="divorced"], c(1), na.rm = TRUE)
+  mean(vipcls$momage[vipcls$marstat=="widowed"], na.rm = TRUE) 
+  sd(vipcls$momage[vipcls$marstat=="widowed"], na.rm = TRUE) 
+  quantile(vipcls$momage[vipcls$marstat=="widowed"], c(1), na.rm = TRUE)
+  mean(vipcls$momage[vipcls$marstat=="never married"], na.rm = TRUE) 
+  sd(vipcls$momage[vipcls$marstat=="never married"], na.rm = TRUE) 
+  quantile(vipcls$momage[vipcls$marstat=="never married"], c(1), na.rm = TRUE)
+#patient IDs of the 5 subjects whose children had the smallest birthweights
+  print(vipcls[order(vipcls$bw), ]   )
+  
