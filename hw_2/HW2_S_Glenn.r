557373115041178508f33b2ddd8a33dcf6f30dd3
Jessica Dyer <jessica.dyer_intern@leveltenenergy.com>
2022-10-09 19:21:30 -0700

    added file with directory management

---
+++ hw_2/HW2_S_Glenn.r
@@ -0,0 +1,21 @@
+## ---------------------------
+##
+## Script name: 
+##
+## Purpose of script:
+##
+## Author: 
+##
+## Date Created: 
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+
+## Working directory
+current_hw_folder <- "hw_2"
+directory <- paste("~/Repositories/epi_510/", current_hw_folder, sep = '')
+setwd(directory)
