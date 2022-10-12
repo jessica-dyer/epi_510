05bee6b1b6860e442f12969548ccad8f41a6e818
Susan Glenn <susanglenn@Susans-MacBook-Pro-5.local>
2022-10-12 15:27:56 -0700

    adding classwork folder

---
+++ Classwork/Class_10.12.22.R
@@ -0,0 +1,16 @@
+## ---------------------------
+##
+## Script name: Classwork
+##
+## Purpose of script: Classwork & notes
+##
+## Author: Susan Glenn
+##
+## Date Created: 2022-10-12
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
